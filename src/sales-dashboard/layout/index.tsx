cd750ac5595ebd9da4784ecac25513edc26e92ec
Masud <masudrahman.dev@gmail.com>
2024-03-06 09:17:28 +0600

    submit

--- src/sales-dashboard/layout/index.tsx
+++ src/sales-dashboard/layout/index.tsx
@@ -2,22 +2,20 @@ import React, { ReactNode } from "react";
 import Navbar from "../navbar";
 import Sidebar from "../sidebar";
 
-
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-
       <div>
         <Navbar />
         <div className="flex  ">
           <aside className="border hidden  md:block relative ">
             <div className=" fixed  min-w-72 ">
-              {/* <Sidebar /> */}
+              <Sidebar />
             </div>
           </aside>
-          <div className="relative ">
+          <div className="relative  ">
             {/* md:left-72 */}
-            <main className="mt-12 px-6  fixed inset-0   left-0 overflow-y-auto ">
+            <main className="pt-40 md:pt-12  px-6 md:left-72 fixed inset-0   left-0 overflow-y-auto ">
               {children}
             </main>
           </div>
